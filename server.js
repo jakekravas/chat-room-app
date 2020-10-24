aaa67204803ad3822e95bb28beaaabde230dc151
Jake Kravas <jakekravas7@gmail.com>
2020-10-23 19:15:16 -0700

    completed application

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser } = require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -33,16 +33,34 @@ io.on("connection", socket => {
         "message",
         formatMessage(botName, `${user.username} user has joined the chat`)
       );
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      }); 
   });
 
   // Listen for chatMessage (shows up for everyone)
   socket.on("chatMessage", msg => {
-    io.emit("message", formatMessage("USER", msg));
+    const user = getCurrentUser(socket.id);
+
+    io.emit("message", formatMessage(user.username, msg));
   });
 
   // Runs when a client disconnects (shows up for everyone)
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat`));
+
+      // Send users and room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      }); 
+    }
   });
 });
 
--- utils/users.js
+++ utils/users.js
@@ -14,4 +14,19 @@ function getCurrentUser(id) {
   return users.find(user => user.id === id);
 }
 
-module.exports = { userJoin, getCurrentUser }
+// User leaves
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  // if no index is found, it returns -1
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+
+module.exports = { userJoin, getCurrentUser, userLeave, getRoomUsers }
